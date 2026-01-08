8c7a9ecaef68566c3cdaf9113d795d245177bd00
ivan <253731512+yurevichivan2011-afk@users.noreply.github.com>
2026-01-08 17:11:41 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,105 @@
+# print('whats your name?')
+# name = input()
+# print('hello', name)
+# n = int(input())
+# while n != 0:
+#     last_digit = n % 10
+#     n = n // 10
+# n = int(input())
+# while n != 0:
+#     last_digit = n % 10
+#     print(last_digit)
+#     n = n // 10
+# n = int(input())
+# sum = 0
+# product = 1
+# while n % 10:
+#     product = product * last_digit
+#     sum = sum + last_digit
+#     n = n // 10
+# print(product)
+# result = 0
+# for i in range(10):
+#     num = int(input())
+#     if num < 0:
+#         break
+#     result += num
+# print(result)
+# num = int(input())
+# flag = True
+# for i in range(2, num):
+#     if num % i == 0:
+#         flag == False
+#         break
+# if flag == True:
+#     print('Число простое')
+# else:
+#     print('Число сложное')
+# n = int(input())
+# flag = False
+# while n != 0:
+#     last_digit = n % 10
+#     if last_digit ==7:
+#         flag = True
+#         break
+#     n //= 10
+# if flag == True:
+#     print('Число', n, 'содержит чифру 7')
+# else:
+#      print('Число', n, 'не содержит чифру 7')
+# for i in range (1, 101):
+#     if i == 7 or i == 17 or i == 29 or i == 79:
+# for i in range(10):
+#     print(i, end='*')
+#     if i > 6:
+#         break
+# n = int(input())
+# flag = False
+# while n != 0:
+#      last_digit = n % n
+#      if last_digit ==1:
+#          flag = True
+#          break
+#          n //= n
+# if flag == True:
+#      print('Число', n, 'содержит чифру 1')
+# else:
+#      print('Число', n, 'не содержит чифру 1')
+
+
+
+#
+# for i in range (1, 101):
+#     if i == 7 or i == 17 or i == 29 or i == 79:
+#         continue
+#     print(i)
+# a = int(input())
+# b = 0
+# c = 0
+# while a != -1:
+#     if a > 7:
+# for hours in range(24):
+#     for minutes in range(60):
+#         for seconds in range(60):
+#             print(hours, minutes, seconds)
+# for i in range(3):
+#     for j in range(3):
+#         print(i,J)
+# for i in range(1,4):
+#     for j in range(3,5):
+#         print(i+3, end='')
+# counter = 0
+# for i in range(99, 102):
+#     temp = i
+#     while temp > 0:
+#         counter += 1
+#         temp //= 10
+# print(counter)
+n = int(input())
+for i in range(n+1):
+    for j in range(3):
+        print(n, end=" ")
+    print()
+
+
+
