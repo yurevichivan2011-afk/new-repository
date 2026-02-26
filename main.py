2397ed8fb9cdb9fa8c02ee455aa799d4ab3ff557
ivan <253731512+yurevichivan2011-afk@users.noreply.github.com>
2026-02-26 18:45:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,77 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+COLOR_WHITE = (255, 0, 0)
+COLOR_YELLOW = (255, 255, 102)
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (213, 50, 80)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (50, 153, 213)
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((400, 400))
-pygame.display.set_caption('My Game')
-clock = pygame.time.Clock()
-running = True
-while running:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill(BLACK)
-    pygame.display.flip()
-pygame.quit()
+SNAKE_BLOCK = 10
+SNAKE_SPEED = 15
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Движущийся квадрат')
+screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+pygame.display.set_caption('Змейка: Улучшеная версия')
+
 clock = pygame.time.Clock()
 
-running = True
-while running:
-    clock.tick(FPS)
+font_style = pygame.font.SysFont('bahnschift', 25)
+score_font0 = pygame.font.SysFont('comicsansms', 35)
+
+def display_score(score):
+    value = score_font.render(f'Ваш счет: {score}', True, COLOR_YELLOW)
+    screen.blit(value, [10, 10])
+
+def draw_snake(block_size, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, COLOR_BLACK, [x[0], x[1], block_size, block_size])
+
+def show_massage(msg, color):
+    mesg = font_style.render(msg, True, color)
+    text_rect = mesg.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_WIDTH / 3))
+    screen.blit(mesg, text_rect)
+
+def generate_food():
+    foodx = round(random.randrange(0, SCREEN_WIDTH - SNAKE_BLOCK)) / 10.0) * 10.0
+    foody = round(random.randrange(0, SCREEN_HEIGHT - SNAKE_BLOCK)) / 10.0) * 10.0
+    return foodx, foody
+
+def game_loop():
+    game_over = False
+    game_close = False
+    x1 = SCREEN_WIDTH / 2
+    y1 = SCREEN_HEIGHT / 2
+    x1_change = 0
+    y1_change = 0
+
+snake_list = []
+length_of_snake = 1
+foodx, foody, = generate_food()
+
+while not game_over:
+    while game_close:
+        screen.fill(COLOR_BLUE)
+        show_massage('Вы програли! Q - выход, C - играть снова', COLOR_RED)
+        display_score(length_of_snake - 1)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    game.over = True
+                    game_close = False
+                     if event.key == pygame.K_q:
+                         game.loop()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-rect_size = 50
-x = WIDTH
-y = HEIGHT
-speed_x = 5
-speed_y = 5
-
-x += speed_x
-y += speed_y
-
-if x + rect_size > WIDTH or x < 0:
-    speed_x = -speed_x
-if y + rect_size > HEIGHT or y < 0:
-    speed_y = -speed_y
-
-rect_size = 50
-x = WIDTH
-y = HEIGHT
-speed_x = 5
-speed_y = 5
-
-screen.fill(BLACK)
-pygame.draw.rect(screen, BLUE, (x, y, rect_size))
-pygame.display.flip()
-pygame.quit()
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and x1_change == 0:
+                x1_change = -SNAKE_BLOCK
+                y1_change = 0
+            elif event
